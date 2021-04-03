625214a6b0e209aca7fdced6ee8fd46dff44dfef
Satoru Kitaguchi <rule.the.fate.myfirststory@gmail.com>
2021-04-04 06:32:50 +0900

    [add]client side

---
+++ SocketSample/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule} from '@angular/common/http';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { SocketService } from "./socket.service";
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+  ],
+  providers: [SocketService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
